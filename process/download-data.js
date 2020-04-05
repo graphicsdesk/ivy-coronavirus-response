f500ee1d058725556ac3b23c9f9ac755caa942d4
Jason Kao <jason.kao@columbia.edu>
2020-04-05 11:48:44 -0400

    calculate days since 100th case for each country

--- process/download-data.js
+++ process/download-data.js
@@ -6,14 +6,21 @@ const COUNTRIES = [ 'US', 'Italy', 'China', 'Korea, South' ];
 
 async function main() {
   // Get data and filter by countries of interest
+  // Fetch data
   const response = await axios.get(DATA_URL);
+
+  // Filter out countries and calculate number of days since 100th case
+  const daysSince100 = {};
   const data = response.data
     .filter(d => COUNTRIES.includes(d.Country))
-    .map(d => ({
-      cases: d.Confirmed,
-      country: d.Country,
-      date: d.Date,
-    }));
+    .map((d, i) => {
+      const { Confirmed: cases, Country: country, Date: date } = d;
+      if (country in daysSince100)
+        daysSince100[country]++;
+      else if (cases >= 100)
+        daysSince100[country] = 0;
+      return { cases, country, date, dayNumber: daysSince100[country] };
+    });
 
   // Write to /data/covid.json
   await fs.writeFile(process.cwd() + '/data/covid.json', JSON.stringify(data, null, 2));
