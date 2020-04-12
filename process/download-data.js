f66c6b78a31bacbcea65a965cfd1801bfa18705f
Jason Kao <jason.kao@columbia.edu>
2020-04-12 10:06:31 -0400

    better data retriever using data.js

--- process/download-data.js
+++ process/download-data.js
@@ -1,17 +1,18 @@
 const fs = require('fs').promises;
-const axios = require('axios');
+const { Dataset } = require('data.js');
 
-const DATA_URL = 'https://pkgstore.datahub.io/core/covid-19/countries-aggregated_json/data/b5df6eb8975c7721162a8cfef5b6ff56/countries-aggregated_json.json';
+const PATH = 'https://datahub.io/core/covid-19/datapackage.json';
 const COUNTRIES = [ 'US', 'Italy', 'China', 'Korea, South' ];
 
 async function main() {
-  // Get data and filter by countries of interest
-  // Fetch data
-  const response = await axios.get(DATA_URL);
+
+  // Retrieve data
+  const { resources } = await Dataset.load(PATH);
+  const file = Object.values(resources).find(r => r._descriptor.name === 'countries-aggregated_json');
 
   // Filter out countries and calculate number of days since 100th case
   const daysSince100 = {};
-  const data = response.data
+  const data = JSON.parse(await file.buffer)
     .filter(d => COUNTRIES.includes(d.Country))
     .map(d => {
       const { Confirmed: cases, Country: country, Date: date } = d;
